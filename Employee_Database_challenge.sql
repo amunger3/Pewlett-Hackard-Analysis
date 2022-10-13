3399e352d010e61f7be1b00589e43d3f087f32d0
Alex Munger <munger.alex@gmail.com>
2022-10-12 22:30:19 -0400

    Challenge code updated

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,14 +1,4 @@
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
-
 SELECT DISTINCT ON (e.emp_no)
 	e.emp_no,
     e.first_name,
@@ -16,13 +6,16 @@ SELECT DISTINCT ON (e.emp_no)
 	t.title,
 	t.from_date,
 	t.to_date
+
 INTO retirement_titles
+
 FROM employees as e
+
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 INNER JOIN dept_emp as de
 ON (e.emp_no = de.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY e.emp_no DESC;
 
-SELECT * from retirement_titles;
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
+ORDER BY e.emp_no DESC;
