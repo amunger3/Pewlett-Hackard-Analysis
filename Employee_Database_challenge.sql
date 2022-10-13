12209675d9a2a84d7ae635abcea2bc5970eaf0b5
Alex Munger <munger.alex@gmail.com>
2022-10-12 22:55:57 -0400

    d1 complete with csv

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,5 +1,6 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (e.emp_no)
+-- Deliverable 1.1
+-- retirement_titles table creation
+SELECT
 	e.emp_no,
     e.first_name,
 	e.last_name,
@@ -18,4 +19,29 @@ ON (e.emp_no = de.emp_no)
 
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 
-ORDER BY e.emp_no DESC;
+ORDER BY e.emp_no ASC;
+
+-- Deliverable 1.2
+-- Use Dictinct with Orderby to remove duplicate rows
+-- unique_titles table creation (also filters for current employees -> to_date = '9999-01-01')
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+    e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+
+INTO unique_titles
+
+FROM employees as e
+
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (t.to_date = '9999-01-01')
+
+ORDER BY e.emp_no ASC, t.to_date DESC;
--- Queries/queries.sql
+++ Queries/queries.sql
@@ -140,6 +140,7 @@ SELECT DISTINCT ON (t.emp_no)
 	t.title
 FROM titles as t;
 
+-- Deliverable 1.1
 SELECT DISTINCT ON (e.emp_no)
 	e.emp_no,
     e.first_name,
@@ -158,3 +159,21 @@ ORDER BY e.emp_no;
 
 SELECT * from retirement_titles;
 
+-- Deliverable 1.2
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+    e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO unique_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * from unique_titles;
